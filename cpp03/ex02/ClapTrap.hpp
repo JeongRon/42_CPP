a7d1e4e0e424c91aa1fcd867811d282c366c4bbe
JeongRon <ljr4081@gmail.com>
2023-12-09 14:20:35 +0900

    [feat] cpp03/ex02

---
+++ cpp03/ex02/ClapTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+ protected:
+  std::string name;
+  unsigned int hitPoint;
+  unsigned int energyPoint;
+  unsigned int attackDamage;
+
+ public:
+  ClapTrap();
+  ClapTrap(std::string name);
+  ~ClapTrap();
+  ClapTrap(ClapTrap& clapTrap);
+  ClapTrap& operator=(const ClapTrap& clapTrap);
+
+  virtual void attack(const std::string& target);
+  // void attack(const std::string& target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ cpp03/ex02/FragTrap.cpp
@@ -0,0 +1,57 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() {
+  name = "FrAgTrAp";
+  hitPoint = 100;
+  energyPoint = 100;
+  attackDamage = 30;
+  std::cout << "FragTrap Constructor Called" << std::endl;
+}
+
+FragTrap::FragTrap(std::string name) {
+  this->name = name;
+  hitPoint = 100;
+  energyPoint = 100;
+  attackDamage = 30;
+  std::cout << "FragTrap Constructor " << name << " Called" << std::endl;
+}
+
+FragTrap::~FragTrap() {
+  std::cout << "FragTrap Destructor Called" << std::endl;
+}
+
+FragTrap::FragTrap(FragTrap& fragTrap) {
+  name = fragTrap.name;
+  hitPoint = fragTrap.hitPoint;
+  energyPoint = fragTrap.energyPoint;
+  attackDamage = fragTrap.attackDamage;
+  std::cout << "FragTrap Copy Constructor Called" << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& fragTrap) {
+  name = fragTrap.name;
+  hitPoint = fragTrap.hitPoint;
+  energyPoint = fragTrap.energyPoint;
+  attackDamage = fragTrap.attackDamage;
+  std::cout << "FragTrap Operator= Called" << std::endl;
+  return (*this);
+}
+
+void FragTrap::highFivesGuys(void) {
+  std::cout << "---------------------" << std::endl;
+  int flag = 0;
+  if (hitPoint == 0) {
+    std::cout << "FragTrap " << name << " HighFivesGuys: There's No hitPoint !"
+              << std::endl;
+    flag = 1;
+  }
+  if (energyPoint == 0) {
+    std::cout << "FragTrap " << name
+              << " HighFivesGuys: There's No energyPoint !" << std::endl;
+    flag = 1;
+  }
+  if (flag == 0) {
+    std::cout << "FragTrap " << name << " HighFivesGuys !" << std::endl;
+  }
+  std::cout << "---------------------" << std::endl;
+}
