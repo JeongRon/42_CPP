f937e9b343dba74d3b87ddfbc0c93c2cf88ace7e
JeongRon <ljr4081@gmail.com>
2023-12-10 22:56:45 +0900

    [feat] cpp04/ex01 : main test -> operator= 깊은 복사 예시 추가

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -33,32 +33,44 @@ int main() {
   std::cout << "----------------3-----------------" << std::endl;
   Cat* tmp1 = new Cat();
   std::string str;
+  // tmp1 idea 값 바꾸기
   for (int i = 0; i < 10; i++) {
     str = "SET" + std::to_string(i);
     tmp1->getBrain()->setIdeas(i, str);
   }
-  // Brain idea 값 바꾸기
+  // tmp1 idea 값 출력
+  std::cout << "-----------Change Check tmp1--------------" << std::endl;
   for (int i = 0; i < 20; i++)
     std::cout << tmp1->getBrain()->getIdeas(i) << std::endl;
-  std::cout << "-----------tmp2 = tmp1-----------------" << std::endl;
+  std::cout << "--------tmp2, tmp3 -> tmp1 복사-----------" << std::endl;
   // Brain 깊은 복사 체크
-  // (1) tmp2 -> tmp1을 복사
+  // (1) tmp2 -> tmp1을 복사 / tmp3 -> tmp1을 복
   Cat* tmp2 = new Cat(*tmp1);
-  // (2) tmp2의 idea 값들을 변경
+  Cat* tmp3 = new Cat();
+  *tmp3 = *tmp1;
+  // (2) tmp2의 idea 값 변경 / tmp3의 idea 값 변경
   for (int i = 0; i < 8; i++) {
     str = "DEEP CHECK" + std::to_string(i);
     tmp2->getBrain()->setIdeas(i, str);
   }
-  // (3) idea 값들 확인 [얕은 복사면, tmp1도 변경 됨]
+  for (int i = 0; i < 15; i++) {
+    str = "DEEP CHECK" + std::to_string(i);
+    tmp3->getBrain()->setIdeas(i, str);
+  }
+  // (3) tmp1, tmp2, tmp3 idea 값 확인
   std::cout << "--------DEEP CHECK (tmp1)-------------" << std::endl;
   for (int i = 0; i < 20; i++)
     std::cout << tmp1->getBrain()->getIdeas(i) << std::endl;
   std::cout << "--------DEEP CHECK (tmp2)-------------" << std::endl;
   for (int i = 0; i < 20; i++)
     std::cout << tmp2->getBrain()->getIdeas(i) << std::endl;
+  std::cout << "--------DEEP CHECK (tmp3)-------------" << std::endl;
+  for (int i = 0; i < 20; i++)
+    std::cout << tmp3->getBrain()->getIdeas(i) << std::endl;
   std::cout << "----------------------------------" << std::endl;
   delete tmp1;
   delete tmp2;
+  delete tmp3;
   std::cout << "----------------------------------" << std::endl;
 
   return 0;
